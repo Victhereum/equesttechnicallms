4e3d3915f3ed68523ff1b017a0c4c7b4ec9d3617
Victor Isaac <tamsvictorisaac@gmail.com>
2022-07-26 01:57:15 +0100

    Testing

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ if READ_DOT_ENV_FILE:
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = False
+DEBUG = True
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
